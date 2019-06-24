436585d4a93c429240b0f2c40d7a2970393f9b4b
Michelle Huang <michelledhuang@g.ucla.edu>
2019-06-23 20:34:44 -0700

    Add user name to schema

--- schema.sql
+++ schema.sql
@@ -7,8 +7,10 @@ USE blogger;
 CREATE TABLE posts (
   post_id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
   user_id int NOT NULL,
+  user_name varchar(100),
   content text,
-  tags varchar(255)
+  tags varchar(255),
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 /*  Execute this file from the command line by typing:
