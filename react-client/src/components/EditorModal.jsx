07ddfe7070391efd1efdaaae10993294b0be3368
Michelle Huang <michelledhuang@g.ucla.edu>
2019-06-23 21:00:01 -0700

    Add create button

--- react-client/src/components/EditorModal.jsx
+++ react-client/src/components/EditorModal.jsx
@@ -2,6 +2,33 @@ import React from 'react';
 import Modal from 'react-modal';
 import OutputBox from './OutputBox.jsx';
 import InputBox from './InputBox.jsx';
+import styled from 'styled-components';
+
+const InputContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  border: 0.05em solid rgb(38, 50, 55);
+  border-radius: 10px;
+  overflow: hidden;
+`;
+
+const CreateButton = styled.button`
+  margin: 10px;
+  width: 120px;
+  height: 40px;
+  border: 2px solid #6b778d;
+  color: #6b778d;
+  background-color: #ffffff;
+  border-radius: 3px;
+  font-size: 15px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #6b778d;
+    border: none;
+    color: #ffffff;
+  }
+`;
 
 const modalStyles = {
   content : {
@@ -12,8 +39,8 @@ const modalStyles = {
     marginRight: '-50%',
     transform: 'translate(-50%, -50%)',
     overflow: 'hidden',
-    border: '0.05em solid rgb(38, 50, 55)',
-    padding: 'none',
+    border: 'none',
+    padding: '15px',
   }
 };
 
@@ -49,9 +76,12 @@ class EditorModal extends React.Component {
         style={modalStyles}
         contentLabel="Editor"
         >
-        <div style={{"display": "flex", "flexDirection": "row"}}>
-          <InputBox onChange={this.onInputChange}/>
-          <OutputBox text={this.state.text}/>
+        <div style={{"display": "flex", "flexDirection": "column", "alignItems": "center"}}>
+          <InputContainer>
+            <InputBox onChange={this.onInputChange}/>
+            <OutputBox text={this.state.text}/>
+          </InputContainer>
+          <CreateButton>Create</CreateButton>
         </div>
       </Modal>
     );
