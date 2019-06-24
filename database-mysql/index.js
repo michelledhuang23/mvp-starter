dde10e1df09a67e515cd477031de5faeb956e06e
Michelle Huang <michelledhuang@g.ucla.edu>
2019-06-24 00:10:31 -0700

    Add user table and inner join

--- database-mysql/index.js
+++ database-mysql/index.js
@@ -7,7 +7,7 @@ var connection = mysql.createConnection({
 });
 
 var selectByID = function(id, callback) {
-  connection.query(`SELECT * FROM posts WHERE user_id = ${id} ORDER BY created_at DESC`, function(err, results, fields) {
+  connection.query(`SELECT users.user_id AS user_id, users.user_name AS user_name, posts.content AS content, posts.tags AS tags, posts.created_at AS created_at FROM users INNER JOIN posts ON users.user_id = posts.user_id WHERE users.user_id = ${id} ORDER BY posts.created_at DESC;`, function(err, results, fields) {
     if(err) {
       callback(err, null);
     } else {
@@ -17,7 +17,7 @@ var selectByID = function(id, callback) {
 };
 
 var createPost = function(post, callback) {
-  connection.query(`INSERT INTO posts (user_id, user_name, content, tags) VALUES (${post.user_id}, "${post.user_name}", "${post.content}", "${post.tags}");`, function(err, results, fields) {
+  connection.query(`INSERT INTO posts (user_id, content, tags) VALUES (${post.user_id}, '${post.content}', '${post.tags}');`, function(err, results, fields) {
     if(err) {
       callback(err, null);
     } else {
--- schema.sql
+++ schema.sql
@@ -4,15 +4,23 @@ CREATE DATABASE blogger;
 
 USE blogger;
 
+CREATE TABLE users (
+  user_id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  user_name varchar(100)
+);
+
 CREATE TABLE posts (
   post_id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
   user_id int NOT NULL,
-  user_name varchar(100),
   content text,
   tags varchar(255),
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (user_id)
+      REFERENCES users(user_id)
+      ON DELETE CASCADE
 );
 
+
 /*  Execute this file from the command line by typing:
- *    mysql -u root < server/schema.sql
+ *    mysql -u root < schema.sql
  *  to create the database and the tables.*/
