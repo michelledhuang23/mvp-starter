532fbe0a7a769a1d6267358c9aae3427eebdc3c9
Michelle Huang <michelledhuang@g.ucla.edu>
2019-06-23 17:42:26 -0700

    Add modal skeleton

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -1,16 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Modal from 'react-modal';
 import BlogPost from './components/BlogPost.jsx';
 import Header from './components/Header.jsx';
 import InputBox from './components/InputBox.jsx';
+
+const modalStyles = {
+  content : {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)'
+  }
+};
+
+Modal.setAppElement('#app');
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = { 
       text: '',
+      modalIsOpen: false,
     }
+
     this.onInputChange = this.onInputChange.bind(this);
+    this.openModal = this.openModal.bind(this);
+    this.afterOpenModal = this.afterOpenModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
   }
 
   onInputChange(e) {
@@ -19,9 +38,30 @@ class App extends React.Component {
     });
   }
 
+  openModal() {
+    this.setState({modalIsOpen: true});
+  }
+
+  afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    // this.subtitle.style.color = '#f00';
+  }
+
+  closeModal() {
+    this.setState({modalIsOpen: false});
+  }
+
   render () {
     return (<div>
       <Header/>
+      <button onClick={this.openModal}>Compose</button>
+      <Modal
+          isOpen={this.state.modalIsOpen}
+          onAfterOpen={this.afterOpenModal}
+          onRequestClose={this.closeModal}
+          style={modalStyles}
+          contentLabel="Editor"
+        ></Modal>
       <div style={{"display": "flex", "flexDirection": "row"}}>
         <InputBox onChange={this.onInputChange}/>
         <BlogPost text={this.state.text}/>
